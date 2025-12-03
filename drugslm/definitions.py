1ef8d59c28eee472cbc65863dbb4bb11dfcdb524
Vinícius de Lima Gonçalves <viniciusslimaa@outlook.com>
2025-12-03 20:12:16 +0000

    improved dagster settings and activate setup_loging in dagster entrypoint

--- drugslm/definitions.py
+++ drugslm/definitions.py
@@ -2,6 +2,9 @@ from dagster import Definitions
 
 from drugslm.scraper.anvisa.pipelines import anvisa_catalog, anvisa_metadata
 from drugslm.scraper.selenium import webdriver_resource
+from drugslm.utils.logging import setup_logging
+
+setup_logging()
 
 defs = Definitions(
     assets=[
